3f2c46057b2281f9dd89895940e6c0afd7c89c36
Max Coplan <mchcopl@gmail.com>
2019-11-12 15:02:20 -0500

    Better cipher

--- Caesar.scala
+++ Caesar.scala
@@ -2,23 +2,19 @@ import scala.util.Random
 object Caesar {
   def main(args: Array[String]) {
     val alpha: Vector[Char] =
-      Vector('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+      ('A' to 'Z').toVector
+    type Cipher = Map[Char, Char]
+    val message: String = "MESSAGE BASIC"
     val cip: Vector[Char] = Random.shuffle(alpha)
+    def makeMap(cip: Vector[Char]): Cipher = (alpha zip cip).toMap
 
-    val message: String = "MESSAGE"
+    def crypt(message: String, cip: Vector[Char]): String = {
+      val map = makeMap(cip)
+      message.toUpperCase.map(c => map.getOrElse(c, c))
+    }
+    println(cip)
 
     println(crypt(message, cip))
 
-    def crypt(message: String, cip: Vector[Char]): String = {
-      val map: Map[Char, Char] = makeMap(alpha, cip)
-      message.map(c => map(c))
-    }
-    def makeMap(alpha: Vector[Char], cip: Vector[Char]): Map[Char, Char] = {
-      val ret: Map[Char, Char] = (alpha.zip(cip)).toMap
-      println(ret)
-      ret
-
-    }
   }
 }
